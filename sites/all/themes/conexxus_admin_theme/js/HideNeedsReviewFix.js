5fb5898761f2240253da075c5229867a8cbfe5a0
mmcintosh <mmcintosh@infowall.com>
2016-04-26 20:17:48 +0000

    fix for hiding upload media popup after use

--- sites/all/themes/conexxus_admin_theme/js/HideNeedsReviewFix.js
+++ sites/all/themes/conexxus_admin_theme/js/HideNeedsReviewFix.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function() {
+ jQuery('.ui-icon-closethick').trigger('click');
  jQuery("#edit-workbench-moderation-state-new option[value=needs_review]").remove();
+});
+
+jQuery( document ).ajaxComplete(function() {
+ jQuery('.ui-icon-closethick').trigger('click');
 });
