2115d47e865fd02aadeaa18c31dac71eb3e997fa
mmcintosh <mmcintosh@infowall.com>
2016-04-26 20:19:37 +0000

    same fix as earlier for media upload popup to close after single use

--- sites/all/themes/si_conexxus/js/HideNeedsReview.js
+++ sites/all/themes/si_conexxus/js/HideNeedsReview.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function() {
+ jQuery('.ui-icon-closethick').trigger('click');
  jQuery("#edit-workbench-moderation-state-new option[value=needs_review]").remove();
  
  jQuery("#main-nav").prepend('<span class="menu-icon"></span>');
