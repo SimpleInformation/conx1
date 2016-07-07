d5e7c3ec807d27504367391310cbba24a38e5e57
Mark McIntosh <mark@simpleinformation.com>
2016-07-07 19:04:29 +0000

    remove media folder option from upload

--- sites/all/themes/conexxus_admin_theme/js/HideNeedsReviewFix.js
+++ sites/all/themes/conexxus_admin_theme/js/HideNeedsReviewFix.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function() {
  jQuery('.ui-icon-closethick').trigger('click');
  jQuery("#edit-workbench-moderation-state-new option[value=needs_review]").remove();
+ jQuery("#edit-field-folder-und option[value=16]").remove();
 });
 
 jQuery( document ).ajaxComplete(function() {
+ jQuery("#edit-field-folder-und option[value=16]").remove();
  if(jQuery('body').hasClass('page-node')) {
    jQuery('.ui-icon-closethick').trigger('click');
  }
